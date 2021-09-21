6f0060eda387a4c7272f4fe536cdfa5ea2e753da
eternallydetermined <87247793+eternallydetermined@users.noreply.github.com>
2021-09-21 00:42:21 -0500

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 1.0f;
+
+    private float timestamp;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time >= timestamp && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + delay;
+        }
+    }
+}
